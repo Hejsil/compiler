ce19a68b1df177ac1058603d32ae7fe541685401
jimmi <rainbowhejsil@gmail.com>
2016-12-26 21:33:50 +0100

    Changes: - Removed a guideline - Removed map from compiler, and just used strcmp instead, as a fast solution. - Added a test program to main. - Fix a small bug in Dynamic_Array - Added Message, that should be used by all modules of the compiler to report errors, warnings and other messages. - Added a function that allocates a string copy using strlen. - A little more work on the parsing. - Refactored some function names

---
+++ src/basic/message.c
@@ -0,0 +1,5 @@
+//
+// Created by jimmi on 12/26/16.
+//
+
+#include "message.h"
---
+++ src/basic/message.h
@@ -0,0 +1,8 @@
+//
+// Created by jimmi on 12/26/16.
+//
+
+#ifndef COMPILER_MESSAGE_H
+#define COMPILER_MESSAGE_H
+
+#endif //COMPILER_MESSAGE_H
